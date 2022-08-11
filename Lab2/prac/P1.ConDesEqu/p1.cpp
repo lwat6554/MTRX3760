9c4116556a3d16eac8291878aae080dd47bd7130
lwat6554 <lwat6554@uni.sydney.edu.au>
2022-08-11 12:47:39 +1000

    working on prac

---
+++ Lab2/prac/P1.ConDesEqu/p1.cpp
@@ -0,0 +1,26 @@
+// SID: 500490778
+// 11/2022
+
+#include <iostream>
+
+class CPlanet {
+    public:
+
+
+
+    private:
+
+
+};
+
+class CCountry: public CPlanet {
+    public:
+
+    private:
+    bool _OnEquator
+};
+
+
+int main() {
+
+}
